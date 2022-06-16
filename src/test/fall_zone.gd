c0a89fbed0e642bfefa41f21fb748bee9ec499da
streq <strequena@gmail.com>
2022-06-16 14:40:20 -0300

    linear damp al agua

--- src/test/fall_zone.gd
+++ src/test/fall_zone.gd
@@ -10,6 +10,9 @@ const skipping_speed_squared = 750*750
 func _physics_process(delta):
 	for _body in get_overlapping_bodies():
 		var body : RigidBody2D = _body
+		#this sucks but it's cause Area2D's linear_damp seems to be broken
+		body.apply_central_impulse(-body.linear_velocity*delta*5)
+
 		if body.linear_velocity.length_squared() < skipping_speed_squared:
 			var splash : CPUParticles2D = SPLASH_PARTICLES.instance()
 			splash.emitting = true
@@ -17,7 +20,6 @@ func _physics_process(delta):
 			body.sink()
 			body.add_child(splash)
 			
-
 
 
 func _on_water_zone_body_entered(body:RigidBody2D):
@@ -31,11 +33,11 @@ func _on_water_zone_body_entered(body:RigidBody2D):
 		skips[body] = skip
 
 
-
 func _on_water_zone_body_exited(body:RigidBody2D):
 	if skips.has(body):
 		var skip : CPUParticles2D = skips[body]
 		skips.erase(body)
 		skip.one_shot = true
-#	if body.sinking:
-#		body.queue_free()
+		
+	if body.sinking:
+		body.queue_free()
